607726cb996cafaddeb8584959d06b1966f4a6fe
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-13 14:34:22 +0200

    add program

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyautogui as pg
+import time
+time.sleep(2)
+
+
+
+# pg.moveTo(200, 300, 0.5)
+
+# pg.drag(50, 50, 0.5, button='left')
+
+# pg.drag(100, 20, duration=0.5)
+
+# print(pg.position())
+
+pg.click(1219, 74)
+
+pg.typewrite('itproger', 0.5)
+pg.typewrite(["enter"])
