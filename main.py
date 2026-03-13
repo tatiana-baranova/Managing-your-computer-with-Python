77ccdb034fb01aa1135ab924581364d59d6e713a
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-13 14:58:02 +0200

    update

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import pyautogui as pg
 import time
 time.sleep(2)
 
-
-
 # pg.moveTo(200, 300, 0.5)
 
 # pg.drag(50, 50, 0.5, button='left')
@@ -12,7 +10,13 @@ time.sleep(2)
 
 # print(pg.position())
 
-pg.click(1219, 74)
+# pg.click(1219, 74)
 
-pg.typewrite('itproger', 0.5)
-pg.typewrite(["enter"])
+# pg.typewrite('itproger', 0.5)
+# pg.typewrite(["enter"])
+
+# pg.hotkey("winleft")
+# pg.typewrite("www.youtube.com\n", 0.5)
+# pg.typewrite("Music", 0.2)
+# pg.hotkey('winleft', 'up')
+# pg.hotkey('ctrl')
